544ff54ec6be1f3f5564205901a647c629ac5b01
Franco <fcalvo33@gmail.com>
2023-02-23 17:29:34 -0300

    Fin

--- src/components/ButtonIndex/ButtonIndex.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './buttonIndex.css'
-
-export default function ButtonIndex(props) {
-  console.log(props.children);
-  return (
-    <button className={props.Style}>{props.text || props.children}</button>
-  )
-}
